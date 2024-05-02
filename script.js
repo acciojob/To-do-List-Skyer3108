2a005423d46122cd280f78260aa83e1cdf9b7b2b
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-02 23:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here
 
+const btn=document.getElementById('addTodoBtn')
+
+const todo=document.getElementById('newTodoInput')
+
+const list=document.getElementById('todoList')
+
+btn.addEventListener('click',()=>{
+
+
+	let lis=document.createElement('li')
+
+	lis.innerHTML=`
+
+	<p>${todo.value}</p>
+	
+	`
+
+		todo.value=''
+	list.appendChild(lis)
+
+	
+
+	
+	
+	
+})
+
