833cfd5e6b7c522080f36c1bd5c7e9736a50e2ae
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-08 00:41:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ const list=document.getElementById('todoList')
 
 btn.addEventListener('click',()=>{
 
-
-	const li=document.createElement('li')
+ 
+	// const li=document.createElement('li')
+	const li=document.getElementById('li')
 
 	li.textContent=`${todo.value}`
 
-	list.appendChild(li)
+	// list.appendChild(li)
 
 		todo.value=''
 	
